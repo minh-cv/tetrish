0e71f1b4301b8f6053214ebc915e4dd2a5976b14
Can Viet Minh <canvietminh@gmail.com>
2026-07-20 09:59:06 +0800

    refactor: separate reload_config

--- src/tetrisd/config_var.h
+++ src/tetrisd/config_var.h
@@ -1,6 +1,10 @@
 #ifndef TETRISH_TETRISD_CONFIG_VAR_H
 #define TETRISH_TETRISD_CONFIG_VAR_H
 
+#include "epollmanip.h"
+#include "tetrissh.h"
+#include <sys/epoll.h>
+
 struct config_var {
     int port;
     char* address;
@@ -12,5 +16,6 @@ struct config_var {
 };
 int config_var_init(struct config_var* cfg_var);
 void config_var_free(struct config_var* cfg);
+void reload_config(struct config_var* cfg, TetrishCredential* credential, struct epoll_event* events, Client* clients);
 
 #endif
