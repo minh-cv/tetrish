906645f34002176b6b7d2ec6170d7869049bad68
Can Viet Minh <canvietminh@gmail.com>
2026-07-22 12:46:32 +0800

    refactor: move functions outside of main & fix reload_config not updating the pointers

--- src/tetrisd/main.c
+++ src/tetrisd/main.c
@@ -2,6 +2,7 @@
 #include "client_io.h"
 #include "config_var.h"
 #include "log_buf.h"
+#include "tetris_client.h"
 #ifndef TETRISH_TETRISD_NO_DAEMON
 #include "daemon.h"
 #endif
@@ -239,6 +240,115 @@ static void main_yield_handler(int epoll_fd, Client* c) {
     }
 }
 
+static void accept_until_block(int listen_fd, int max_clients, int epoll_fd, TetrishCredential* credential, Client* clients) {
+    for (;;) {
+        int client_fd = accept(listen_fd, NULL, NULL);
+        if (client_fd == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                break;
+            }
+            if (errno == EINTR) {
+                continue;
+            }
+            LOGGER_PERROR("accept", "accept");
+            break;
+        }
+
+        if (client_fd >= max_clients) {
+            errno = EMFILE;
+            LOGGER_PERROR("accept", "client_fd too large");
+            close(client_fd);
+            continue;
+        }
+
+        if (set_nonblocking(client_fd) == -1) {
+            LOGGER_PERROR("accept", "set_nonblocking");
+            close(client_fd);
+            continue;
+        }
+
+        if ((add_client(&clients[client_fd], epoll_fd, client_fd, credential)) == -1) {
+            LOGGER_PERROR("accept", "add_client");
+            continue;
+        }
+
+        LOGGER_LOG(LOG_INFO, "client", "accept client fd=%d", client_fd);
+    }
+}
+
+static void reload_config(struct config_var* cfg, TetrishCredential* const credential, struct epoll_event** events_ptr, Client** clients_ptr) {
+    struct epoll_event* events = *events_ptr;
+    Client* clients = *clients_ptr;
+
+    LOGGER_LOG(LOG_INFO, "config", "Reconfiguring...");
+    struct config_var tmp_cfg;
+    if (config_var_init(&tmp_cfg) == -1) {
+        return;
+    }
+
+    if (tmp_cfg.port != cfg->port) {
+        LOGGER_LOG(LOG_WARN, "config", "Ignoring new port");
+    }
+
+    if (strcmp(tmp_cfg.address, cfg->address) != 0) {
+        char* tmp = cfg->address;
+        cfg->address = tmp_cfg.address;
+        tmp_cfg.address = tmp;
+    }
+
+    if (strcmp(tmp_cfg.key_path, cfg->key_path) != 0 || strcmp(tmp_cfg.cert_path, cfg->cert_path) != 0) {
+        TetrishCredential new_credential;
+        if (tetrish_credential_init(&new_credential, tmp_cfg.key_path, tmp_cfg.cert_path) == -1) {
+            LOGGER_LOG(LOG_ERROR, "config", "Cannot reconfigure new credential path");
+        }
+        else {
+            tetrish_credential_free(credential);
+            *credential = new_credential;
+        }
+    }
+
+    if (tmp_cfg.max_events != cfg->max_events) {
+        struct epoll_event* new_events = realloc(events, (size_t)tmp_cfg.max_events*sizeof(struct epoll_event));
+        if (new_events == NULL) {
+            // TODO: log 
+        }
+        else {
+            events = new_events;
+            cfg->max_events = tmp_cfg.max_events;
+        }
+    }
+
+    if (strcmp(tmp_cfg.log_ipc, cfg->log_ipc) != 0) {
+        char* tmp = tmp_cfg.log_ipc;
+        tmp_cfg.log_ipc = cfg->log_ipc;
+        cfg->log_ipc = tmp;
+    }
+
+    if (tmp_cfg.max_clients != cfg->max_clients) {
+        if (tmp_cfg.max_clients < cfg->max_clients) {
+            LOGGER_LOG(LOG_ERROR, "config", "Can't reconfig max_clients");
+        }
+        else {
+            Client* new_clients = realloc(clients, (size_t)tmp_cfg.max_clients*sizeof(*new_clients));
+            if (new_clients == NULL) {
+                // TODO: log 
+            }
+            else {
+                clients = new_clients;
+                memset(&new_clients[cfg->max_clients], 0, (size_t)(tmp_cfg.max_clients - cfg->max_clients)*sizeof(Client));
+                cfg->max_clients = tmp_cfg.max_clients;
+            }
+        }
+    }
+
+    *clients_ptr = clients;
+    *events_ptr = events;
+
+    config_var_free(&tmp_cfg);
+    return;
+}
+
+
 int main() {
     DTOR_DEFINE(dtor, 20);
     
@@ -344,37 +454,7 @@ int main() {
             uint32_t evs = events[i].events;
 
             if (fd == listen_fd) {
-                for (;;) {
-                    int client_fd = accept(listen_fd, NULL, NULL);
-                    if (client_fd == -1) {
-                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                            break;
-                        }
-                        if (errno == EINTR) {
-                            continue;
-                        }
-                        LOGGER_PERROR("accept", "accept");
-                        break;
-                    }
-
-                    if (client_fd >= cfg.max_clients) {
-                        errno = EMFILE;
-                        LOGGER_PERROR("accept", "client_fd too large");
-                        close(client_fd);
-                        continue;
-                    }
-
-                    if (set_nonblocking(client_fd) == -1) {
-                        LOGGER_PERROR("accept", "set_nonblocking");
-                        close(client_fd);
-                        continue;
-                    }
-
-                    if ((add_client(&clients[client_fd], epoll_fd, client_fd, &credential)) == -1) {
-                        LOGGER_PERROR("accept", "add_client");
-                        continue;
-                    }
-                }
+                accept_until_block(fd, cfg.max_clients, epoll_fd, &credential, clients);
                 continue;
             }
 
@@ -414,7 +494,6 @@ int main() {
         else if (logger_ctx.has_log) {
             ClientIoResult result = resume_client_event(epoll_fd, &clients[client_logger_fd]);
             if (result == CLIENT_IO_ERR) {
-                LOGGER_LOG(LOG_INFO, "logger", "closing logger fd=%d", client_logger_fd);
                 client_logger_fd = -1;
             }
             if (logger_ctx.log_buf.buffer_size == 0) {
@@ -424,15 +503,12 @@ int main() {
 
         if (should_reload_config) {
             should_reload_config = 0;
-            reload_config(&cfg, &credential, events, clients);
+            reload_config(&cfg, &credential, &events, &clients);
         }
     }
 
     if (client_logger_fd != -1 && logger_ctx.has_log) {
-        ClientIoResult result = resume_client_event(epoll_fd, &clients[client_logger_fd]);
-        if (result == CLIENT_IO_ERR) {
-            LOGGER_LOG(LOG_INFO, "logger", "closing logger fd=%d", client_logger_fd);
-        }
+        resume_client_event(epoll_fd, &clients[client_logger_fd]);
         if (logger_ctx.log_buf.buffer_size == 0) {
             logger_ctx.has_log = false;
         }
